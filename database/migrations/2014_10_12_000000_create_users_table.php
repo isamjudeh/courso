3aa7ec09f6a502f43e77aee2ac7d8bdb1f7b10f7
loaidev <loaidev64@gmail.com>
2023-06-09 18:27:48 +0300

    unique phone

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->text('image')->nullable();
-            $table->string('phone');
+            $table->string('phone')->unique();
             $table->string('first_name');
             $table->string('last_name');
             $table->timestamp('birth_date');
