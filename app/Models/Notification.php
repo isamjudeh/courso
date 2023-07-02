71d76e37fd5c905e46de85ac6815ac312d58ad34
Loai Dev <loaidev64@gmail.com>
2023-07-02 21:24:17 +0300

    edit notification

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -14,6 +14,16 @@ class Notification extends Model
 
     public $fillable = ['course_id', 'registeration_id', 'user_id', 'admin_approved', 'user_approved'];
 
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'admin_approved' => 'boolean',
+        'user_approved' => 'boolean',
+    ];
+
     function course(): BelongsTo
     {
         return $this->belongsTo(Course::class);
