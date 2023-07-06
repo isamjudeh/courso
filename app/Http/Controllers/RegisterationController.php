993312b81f843a995874247ed4e1f1e0a8469398
Loai Dev <loaidev64@gmail.com>
2023-07-06 07:34:07 +0300

    edit registeration

--- app/Http/Controllers/RegisterationController.php
+++ app/Http/Controllers/RegisterationController.php
@@ -4,12 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\RegisterationStoreRequest;
 use App\Models\Registeration;
+use App\Models\Student;
 use Illuminate\Http\Request;
 
 class RegisterationController extends Controller
 {
     public function store(RegisterationStoreRequest $request)
     {
+        $student = Student::where('course_id', $request->course_id)->where('user_id', auth()->id())->first();
+        $notification = auth()->user()->notifications()->where('course_id', $request->course_id)->first();
+        if ($student || $notification) {
+            return response([
+                'message' => 'انت بالفعل قمت بتقديم طلب تسجيل على هذا الكورس'
+            ], 400);
+        }
+
         Registeration::create(array_merge($request->validated(), ['user_id' => auth()->id()]));
 
         return response()->noContent();
